3d68e0f10eb2d9d9fb14db2e5758bcbbe8a6eecd
trumantoy <sl.truman@live.com>
2025-07-16 16:41:57 +0800

    fix: 修改程序名

--- src/app.py
+++ src/app.py
@@ -7,127 +7,6 @@ from gi.repository import GLib, Gtk, Gio, GObject  # 添加 GObject 导入
 
 
 from app_window import *
-
-import ctypes
-from ctypes import wintypes
-
-# 加载 user32.dll
-user32 = ctypes.windll.user32
-
-# 定义 RECT 结构体
-class RECT(ctypes.Structure):
-    _fields_ = [
-        ("left", wintypes.LONG),
-        ("top", wintypes.LONG),
-        ("right", wintypes.LONG),
-        ("bottom", wintypes.LONG)
-    ]
-
-# 定义 FindWindowW 和 GetWindowRect 函数
-FindWindowW = user32.FindWindowW
-FindWindowW.argtypes = [wintypes.LPCWSTR, wintypes.LPCWSTR]
-FindWindowW.restype = wintypes.HWND
-
-GetWindowRect = user32.GetWindowRect
-GetWindowRect.argtypes = [wintypes.HWND, ctypes.POINTER(RECT)]
-GetWindowRect.restype = wintypes.BOOL
-
-# 使用 Win32 API 获取当前活动窗口句柄
-GetActiveWindow = user32.GetActiveWindow
-GetActiveWindow.argtypes = []
-GetActiveWindow.restype = wintypes.HWND
-
-
-# 定义 SetWindowPos 函数
-SetWindowPos = user32.SetWindowPos
-SetWindowPos.argtypes = [wintypes.HWND, wintypes.HWND, wintypes.INT, wintypes.INT,wintypes.INT, wintypes.INT, wintypes.UINT]
-SetWindowPos.restype = wintypes.BOOL
-
-MoveWindow = user32.MoveWindow
-MoveWindow.argtypes = [wintypes.HWND, wintypes.INT, wintypes.INT, wintypes.INT, wintypes.INT, wintypes.BOOL]
-MoveWindow.restype = wintypes.BOOL
-
-ShowWindow = user32.ShowWindow
-ShowWindow.argtypes = [wintypes.HWND, wintypes.INT]
-ShowWindow.restype = wintypes.BOOL
-
-# 新增 SetWindowLong 函数定义
-SetWindowLong = user32.SetWindowLongW
-SetWindowLong.argtypes = [wintypes.HWND, wintypes.INT, wintypes.LONG]
-SetWindowLong.restype = wintypes.LONG
-
-GetWindowLong = user32.GetWindowLongW
-GetWindowLong.argtypes = [wintypes.HWND, wintypes.INT]
-GetWindowLong.restype = wintypes.LONG
-
-# 定义窗口状态常量
-SW_SHOWNORMAL = 1
-SW_SHOWMINIMIZED = 2
-SW_SHOWMAXIMIZED = 3
-
-GWL_EXSTYLE = -20
-WS_EX_TOOLWINDOW = 0x00000080
-SWP_NOSIZE = 0x0001
-SWP_NOMOVE = 0x0002
-SWP_NOACTIVATE = 0x0010
-
-def get_wechat_window_position_and_size():
-    # 微信窗口类名通常为 'WeChatMainWndForPC'，标题可能为空
-    hwnd = FindWindowW("WeChatMainWndForPC", None)
-    if hwnd == 0:
-        print("未找到微信窗口")
-        return None
-
-    rect = RECT()
-    if GetWindowRect(hwnd, ctypes.byref(rect)):
-        width = rect.right - rect.left
-        height = rect.bottom - rect.top
-        return rect.left, rect.top, width, height
-    else:
-        print("无法获取微信窗口位置和大小")
-        return None
-
-
-def update_window_position(app_window):
-    wechat_hwnd = FindWindowW("WeChatMainWndForPC", None)
-    app_hwnd = FindWindowW(None, "私域助手")
-
-    wechat_size = get_wechat_window_position_and_size()
-    wx_left, wx_top, wx_width, wx_height = wechat_size
-    
-    if app_window.is_suspended():
-        return True
-
-    if app_window.get_height() != wx_height:
-        app_window.set_size_request(400, wx_height)
-
-    rect = RECT()
-    GetWindowRect(app_hwnd, ctypes.byref(rect))
-    width = rect.right - rect.left
-    height = rect.bottom - rect.top
-    
-    if rect.left != wx_left + wx_width or rect.top != wx_top - 10:
-        SetWindowPos(app_hwnd, wechat_hwnd, wx_left + wx_width, wx_top - 10, 0,0, SWP_NOSIZE | SWP_NOACTIVATE)
-    else:
-        SetWindowPos(app_hwnd, wechat_hwnd, 0, 0, 0,0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE)
-    
-    # app_hwnd = FindWindowW(None, "微信助手")
-    # current_style = GetWindowLong(app_hwnd, GWL_EXSTYLE)
-    # ex_style = SetWindowLong(app_hwnd, GWL_EXSTYLE, current_style | WS_EX_TOOLWINDOW)
-
-    return True
-
-def do_close_request(sender):
-    sender.unmap()
-    return True
-
-def show_window(app_window):
-    app_window.map()
-
-def quit_window(app_window,hook_close):
-    app_window.disconnect(hook_close)
-    app_window.close()
-
 import threading
 import pystray
 from PIL import Image  
@@ -142,6 +21,18 @@ if __name__ == '__main__':
         builder = Gtk.Builder.new_from_file('ui/app.ui')
         app_window = builder.get_object('app_window')
         app.add_window(app_window)
+
+        def do_close_request(sender):
+            sender.unmap()
+            return True
+
+        def show_window(app_window):
+            app_window.map()
+
+        def quit_window(app_window,hook_close):
+            app_window.disconnect(hook_close)
+            app_window.close()
+
         hook_close = app_window.connect("close-request", do_close_request)
         menu = (pystray.MenuItem('显示', lambda: show_window(app_window), default=True), 
                     pystray.Menu.SEPARATOR, 
@@ -151,9 +42,7 @@ if __name__ == '__main__':
         icon = pystray.Icon("icon", image, "图标名称", menu)
         threading.Thread(target=icon.run, daemon=True).start()
 
-        # GLib.timeout_add(200, update_window_position, app_window)
-
-    app = Gtk.Application(application_id="xyz.building3d.app")
+    app = Gtk.Application(application_id="xyz.assistant.app")
     app.connect('activate',do_activate)
 
     exit_status = app.run(sys.argv)
--- src/app_window.py
+++ src/app_window.py
@@ -6,6 +6,71 @@ import time
 import os
 import subprocess
 
+
+import ctypes
+from ctypes import wintypes
+
+# 加载 user32.dll
+user32 = ctypes.windll.user32
+
+# 定义 RECT 结构体
+class RECT(ctypes.Structure):
+    _fields_ = [
+        ("left", wintypes.LONG),
+        ("top", wintypes.LONG),
+        ("right", wintypes.LONG),
+        ("bottom", wintypes.LONG)
+    ]
+
+# 定义 FindWindowW 和 GetWindowRect 函数
+FindWindowW = user32.FindWindowW
+FindWindowW.argtypes = [wintypes.LPCWSTR, wintypes.LPCWSTR]
+FindWindowW.restype = wintypes.HWND
+
+GetWindowRect = user32.GetWindowRect
+GetWindowRect.argtypes = [wintypes.HWND, ctypes.POINTER(RECT)]
+GetWindowRect.restype = wintypes.BOOL
+
+# 使用 Win32 API 获取当前活动窗口句柄
+GetActiveWindow = user32.GetActiveWindow
+GetActiveWindow.argtypes = []
+GetActiveWindow.restype = wintypes.HWND
+
+
+# 定义 SetWindowPos 函数
+SetWindowPos = user32.SetWindowPos
+SetWindowPos.argtypes = [wintypes.HWND, wintypes.HWND, wintypes.INT, wintypes.INT,wintypes.INT, wintypes.INT, wintypes.UINT]
+SetWindowPos.restype = wintypes.BOOL
+
+MoveWindow = user32.MoveWindow
+MoveWindow.argtypes = [wintypes.HWND, wintypes.INT, wintypes.INT, wintypes.INT, wintypes.INT, wintypes.BOOL]
+MoveWindow.restype = wintypes.BOOL
+
+ShowWindow = user32.ShowWindow
+ShowWindow.argtypes = [wintypes.HWND, wintypes.INT]
+ShowWindow.restype = wintypes.BOOL
+
+# 新增 SetWindowLong 函数定义
+SetWindowLong = user32.SetWindowLongW
+SetWindowLong.argtypes = [wintypes.HWND, wintypes.INT, wintypes.LONG]
+SetWindowLong.restype = wintypes.LONG
+
+GetWindowLong = user32.GetWindowLongW
+GetWindowLong.argtypes = [wintypes.HWND, wintypes.INT]
+GetWindowLong.restype = wintypes.LONG
+
+# 定义窗口状态常量
+SW_SHOWNORMAL = 1
+SW_SHOWMINIMIZED = 2
+SW_SHOWMAXIMIZED = 3
+
+GWL_EXSTYLE = -20
+WS_EX_TOOLWINDOW = 0x00000080
+SWP_NOSIZE = 0x0001
+SWP_NOMOVE = 0x0002
+SWP_NOACTIVATE = 0x0010
+
+
 @Gtk.Template(filename='ui/app_window.ui')
 class AppWindow (Gtk.ApplicationWindow):
     __gtype_name__ = "AppWindow"
@@ -28,7 +93,28 @@ class AppWindow (Gtk.ApplicationWindow):
         
         self.set_titlebar(header)
 
-        model = Gtk.StringList.new(["项目1", "项目2", "项目3"])
+        # 创建右键菜单模型
+        menu = Gio.Menu()
+        menu.append("编辑", "win.edit")
+        
+        # 创建弹出菜单
+        self.popover = Gtk.PopoverMenu()
+        self.popover.set_menu_model(menu)
+
+        # 添加右键点击控制器
+        click_controller = Gtk.GestureClick(button=3)  # 右键
+        def right_clicked(c, n, x, y):
+            rect = Gdk.Rectangle()
+            rect.x = x
+            rect.y = y
+            self.popover.set_pointing_to(rect)
+            self.popover.popup()
+
+        click_controller.connect("pressed", right_clicked)
+        self.add_controller(click_controller)
+        self.popover.set_parent(self)
+
+        model = Gtk.StringList.new(["项目1111", "项目2", "项目3"])
         selection_model = Gtk.SingleSelection.new(model)
         self.listview1.set_model(selection_model)
         
@@ -56,61 +142,73 @@ class AppWindow (Gtk.ApplicationWindow):
         startupinfo.wShowWindow = subprocess.SW_HIDE  # 隐藏窗口
         
         self.wx = subprocess.Popen(['plugins/wx.exe'],
-            text=True,
+            text=True,encoding='gbk',
             stdin=subprocess.PIPE,
             startupinfo=startupinfo)
+        
+
+        GLib.idle_add(self.update_window_position)
+
+    def get_wechat_window_position_and_size(self):
+        # 微信窗口类名通常为 'WeChatMainWndForPC'，标题可能为空
+        hwnd = FindWindowW("WeChatMainWndForPC", None)
+        if hwnd == 0:
+            print("未找到微信窗口")
+            return None
+
+        rect = RECT()
+        if GetWindowRect(hwnd, ctypes.byref(rect)):
+            width = rect.right - rect.left
+            height = rect.bottom - rect.top
+            return rect.left, rect.top, width, height
+        else:
+            print("无法获取微信窗口位置和大小")
+            return None
+
+    def update_window_position(self):
+        if self.is_suspended():
+            return True
+
+        if self.popover.get_visible():
+            return True
+
+        wechat_hwnd = FindWindowW("WeChatMainWndForPC", None)
+        wx_rect = RECT()
+        if not GetWindowRect(wechat_hwnd, ctypes.byref(wx_rect)):    
+            return True
+        
+        wx_height = wx_rect.bottom - wx_rect.top
+        if self.get_height() != wx_height:
+            self.set_size_request(400, wx_height)
+
+        if 'app_hwnd' not in vars(self):
+            self.app_hwnd = FindWindowW('gdkSurfaceToplevel', "私域助手")
+
+        app_hwnd = self.app_hwnd
+
+        rect = RECT()
+        if not GetWindowRect(app_hwnd, ctypes.byref(rect)):
+            return True
+        
+        if rect.left != wx_rect.right or rect.top != wx_rect.top - 10:
+            SetWindowPos(app_hwnd, wechat_hwnd, wx_rect.right, wx_rect.top - 10, 0,0, SWP_NOSIZE)
+        else:
+            SetWindowPos(app_hwnd, wechat_hwnd, 0, 0, 0,0, SWP_NOMOVE | SWP_NOSIZE)
+        return True
 
     def setup_listitem1(self, factory, list_item):
-        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        
-        # 创建右键菜单模型
-        menu = Gio.Menu()
-        menu.append("复制", "app.copy")
-        menu.append("删除", "app.delete")
-        
-        # 创建弹出菜单
-        popover = Gtk.PopoverMenu()
-        popover.set_menu_model(menu)
-        
-        # 添加右键点击控制器
-        click_controller = Gtk.GestureClick(button=3)  # 右键
-        click_controller.connect("pressed", lambda c, n, x, y: 
-            popover.set_parent(box) or 
-            popover.popup())
-        box.add_controller(click_controller)
-        
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+                
         # 添加发送按钮
         send_button = Gtk.Button()
         send_button.set_icon_name("mail-send-symbolic")  # 设置发送图标
         send_button.connect("clicked", self.send_msg_clicked)
         send_button.set_size_request(20, -1)
         box.append(send_button)
-
         label = Gtk.Label()
-        label.set_wrap(True)  # 启用自动换行
-        label.set_halign(Gtk.Align.START)  # 左对齐
         box.append(label)
-
+        
         list_item.set_child(box)
-
-    # 新增右键点击处理方法
-    def on_right_click(self, controller, n_press, x, y):
-        if n_press == 1:  # 右键单击
-            # 创建弹出菜单
-            menu = Gtk.Menu()
-            
-            # 添加菜单项
-            copy_item = Gtk.MenuItem(label="复制")
-            copy_item.connect("activate", self.on_copy)
-            menu.append(copy_item)
-            
-            delete_item = Gtk.MenuItem(label="删除")
-            delete_item.connect("activate", self.on_delete)
-            menu.append(delete_item)
-            
-            menu.show_all()
-            menu.popup_at_pointer(None)  # 在鼠标位置显示菜单
-
 
     def bind_listitem1(self, factory, list_item):
         item = list_item.get_item()
